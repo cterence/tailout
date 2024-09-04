5a6c1436af1946f87a98049473d6f858e5c173fd
Shay Elkin <shay+git@elkin.io>
2024-09-04 14:16:49 -0700

    Add "version" command
    This adds a "version" command that does exactly what you'd expect: prints the (module)
    version of Tailscale, and the hashmap of the commit the build was made from. It takes both
    from the BuildInfo embedded in the binary.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -26,6 +26,7 @@ func buildTailoutCommand(app *tailout.App) *cobra.Command {
 	cmd.AddCommand(buildStatusCommand(app))
 	cmd.AddCommand(buildStopCommand(app))
 	cmd.AddCommand(buildUiCommand(app))
+        cmd.AddCommand(buildVersionCommand(app))
 
 	return cmd
 }
